6dcd508d512cd92f2ba1ccdfcdcc26658e2a2b64
shashafooy <aaron.pettit@comcast.net>
2017-09-29 16:42:51 -0600

    Set up simulator and normal keil versions

---
+++ ARM_Microcontroller/main.s
@@ -0,0 +1,45 @@
+; main.s
+; Runs on any Cortex M processor
+; A very simple first project implementing a random number generator
+; Daniel Valvano
+; May 4, 2012
+
+;  This example accompanies the book
+;  "Embedded Systems: Introduction to Arm Cortex M Microcontrollers",
+;  ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2012
+;  Section 3.3.10, Program 3.12
+;
+;Copyright 2012 by Jonathan W. Valvano, valvano@mail.utexas.edu
+;   You may use, edit, run or distribute this file
+;   as long as the above copyright notice remains
+;THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+;OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+;MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+;VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+;OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+;For more information about my classes, my research, and my books, see
+;http://users.ece.utexas.edu/~valvano/
+
+
+       THUMB
+       AREA    DATA, ALIGN=2
+       ALIGN          
+       AREA    |.text|, CODE, READONLY, ALIGN=2
+       EXPORT  Start
+	   ;unlock 0x4C4F434B
+	   
+	   ;PF4 is SW1
+	   ;PF0 is SW2
+	   ;PF1 is RGB Red
+	   ;Enable Clock RCGCGPIO p338
+	   ;Set direction 1 is out 0 is in. GPIODIR
+	   ;DEN 
+	   ; 0x3FC
+	   
+	   
+Start  
+       B   Start
+
+       ALIGN      
+       END  
+           
