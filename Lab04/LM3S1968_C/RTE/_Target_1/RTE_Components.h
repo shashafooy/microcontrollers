539e5756f67c270488ea2309c7fe9828c77f2e4c
shashafooy <aaron.pettit@comcast.net>
2017-10-23 14:54:06 -0600

    Added initializations for port E,F

--- Lab04/LM3S1968_C/RTE/_Target_1/RTE_Components.h
+++ Lab04/LM3S1968_C/RTE/_Target_1/RTE_Components.h
@@ -11,4 +11,10 @@
 #define RTE_COMPONENTS_H
 
 
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "lm3s1968.h"
+
+
 #endif /* RTE_COMPONENTS_H */
--- Lab04/main.c
+++ Lab04/main.c
@@ -20,28 +20,78 @@ int charSize = 0;
 int startbit = 0;
 char dataStore[1000];
 int skipStore = 0;
+
+volatile unsigned int RCGC2 __attribute__((at(0x400FE108)));
+volatile unsigned int PE_DATA __attribute__((at(0x400243FC)));
+volatile unsigned int PF_DATA __attribute__((at(0x400253FC)));
+volatile unsigned int PF_LOCK __attribute__((at(0x40025520)));
+
 unsigned char *PE = (unsigned char *) 0x40024000;
-unsigned int PE_DATA __attribute__((at(0x400243FC)));
+unsigned char *PF = (unsigned char *) 0x40025000;
+volatile char temp;
+
+#define RCGC2_PE 0x00000010
+#define RCGC2_PF 0x00000020
+#define UNLOCK 0x4C4F434B
 
 
 
-
-void init(){}
+void init(){
+	RCGC2 = RCGC2 | RCGC2_PE | RCGC2_PF; //enable port E
+	
+	PE[0x420] &= 0x00; //turn off alt function
+	PE[0x400] = 0x00; //pin 1,2 input
+	PE[0x51c] |= 0x03; //enable pin 1,2
+	
+	PF_LOCK = UNLOCK;
+	PF[0x524] = 1; //unlock pin 0
+	PF[0x420] &= 0x00; //turn off alt function
+	PF[0x514] = 0x1; //pull down
+	PF[0x400] = 0x0; //PF0 input
+	PF[0x51c] = 0x1; //activate pin 0
+	
+	
+	
+	
+	
+	//interrupt for port E clock pin 1
+	PE[0x404] = 0x0; //interrupt on edge
+	PE[0x408] = 0x0; //disable both edges
+	PE[0x40c] = 0x0; //falling edge
+	PE[0x410] = 0x2; //enable interrupt
+	//PE[0x414] interrupt status
+	
+	PF[0x404] = 0x0;
+	PF[0x408] = 0x0;
+	PF[0x40c] = 0x1; //rising edge
+	PE[0x410] = 0x1; //enable interrupt
+	
+	
+	//UART Hell
+}
 
 	
-void portFInterrupt(void){
+void GPIOF_Handler(void){
+	PF[0x41c] = 0x1; //acknowledge pin 0, SW2
 	sendData();
 }	
 	
 	
-void portEInterrupt(void){ //***********Falling Clock edge*************
-	if(charSize < 11){ //keyboard sends 11 bits at a time(maybe)
-		if(charSize > 0 && charSize < 9){	//recieved start bit
-			keystroke = keystroke >> 1;
-			char temp = PE[0x004] << 7; //shift data left by 7, now 7th bit
-			keystroke |= temp; //store shifted 7th bit of keyboard data
-		}	
-		charSize++;
+void GPIOE_Handler(void){ //***********Falling Clock edge*************
+	switch(PE[0x414]){ //change to interrupt pin
+		case 0x2: //clock interrupt, pin 1
+			PE[0x41c] = 0x2; //acknowledge pin 1 interrupt
+			if(charSize < 11){ //keyboard sends 11 bits at a time(maybe)
+				if(charSize > 0 && charSize < 9){	//recieved start bit
+					keystroke = keystroke >> 1;
+					temp = PE[0x004] << 7; //shift data left by 7, now 7th bit
+					keystroke |= temp; //store shifted 7th bit of keyboard data
+				}	
+				charSize++;
+			}
+			return;
+		default:
+			return;
 	}
 }	
 	
